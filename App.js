9c905ff6cd87a82c18e20e524dffc46a1ae71d5a
Elieth Umaña <izaduartes2214@gmail.com>
2024-08-28 10:52:17 -0600

    Atardeceres

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import Atardecer from './COMPONENTES/Atardecer';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.tituloPrincipal}>Galeria de atardeceres</Text>
+
+      <ScrollView>
+      <View style={styles.imageContainer}>
+        <Atardecer>
+          nombre='Atardecer morado'
+          imagen={require('./IMAGENES/1.jpg')}
+        </Atardecer>
+        <Atardecer>
+          nombre='Atardecer'
+          imagen={require('./IMAGENES/2.jpg')}
+        </Atardecer>
+        <Atardecer>
+          nombre='Atardecer naranja'
+          imagen={require('./IMAGENES/3.jpg')}
+        </Atardecer>
+        <Atardecer>
+          nombre='Atardecer ilustrado'
+          imagen={require('./IMAGENES/4.jpg')}
+        </Atardecer>
+        <Atardecer>
+          nombre='Atardecer y mar'
+          imagen={require('./IMAGENES/5.jpg')}
+        </Atardecer>
+        <Atardecer>
+          nombre='Atardecer babyblue'
+          imagen={require('./IMAGENES/6.jpg')}
+        </Atardecer>
+        <Atardecer>
+          nombre='Atardecer horizonte'
+          imagen={require('./IMAGENES/7.jpg')}
+        </Atardecer>
+        <Atardecer>
+          nombre='Atardecer fantastico'
+          imagen={require('./IMAGENES/8.jpg')}
+        </Atardecer>
+        <Atardecer>
+          nombre='Atardecer lightblue'
+          imagen={require('./IMAGENES/9.jpg')}
+        </Atardecer>
+        <Atardecer>
+          nombre='Atardecer y mar'
+          imagen={require('./IMAGENES/10.jpg')}
+        </Atardecer>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -15,6 +59,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    marginVertical: 20,
+  },
+  tituloPrincipal: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+    marginBottom: 10,
+  },
+  scrollView: {
+    alignItems: 'center',
+    paddingBottom: 20,
+  },
+  imageContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     justifyContent: 'center',
+  },
+  imagen: {
+    borderRadius: 5,
+    width: 150,
+    height: 150,
+  },
+  titulo: {
+    marginTop: 5,
+    fontSize: 16,
+    color: 'black',
   },
 });
---
+++ COMPONENTES/Atardecer.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+
+function Atardecer(props) {
+    return (
+        <View>
+            <Image style={styles.atardecer} source={props.imagen}/>
+            <Text style={styles.titulo} > {props.nombre} </Text>
+        </View>
+    )
+}
+
+export default Atardecer;
+const styles = StyleSheet.create({
+    atardecer:{
+        marginTop:10,
+        borderRadius:8,
+        width:300,
+        height:300,
+    },
+    titulo:{
+        fontSize:12,
+        fontWeight:'bold',
+        color:'blue',
+    },
+});
